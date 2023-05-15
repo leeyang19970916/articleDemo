2d4d470423a95f7ab2cc572686e48e98b55753a4
leeyang19970916 <yang19970916@gmail.com>
2023-05-15 18:02:59 +0800

    AreaManager

--- js/second-navUI.js
+++ js/second-navUI.js
@@ -2,17 +2,28 @@ function secondNavUI(params) {
   let secondNav = document.querySelector(".second-nav");
   let str = "";
   for (let i = 0; i < secondNavArr.length; i++) {
-    
     const element = secondNavArr[i];
-    str += `<button onclick="tabContentChange('${element.id}')" class="nav-link ${i === 0 && "active"}
-     second-nav-item" id="v-pills-${
+    str += `<button onclick="tabContentChange('${
       element.id
-    }-tab" data-bs-toggle="pill" data-bs-target="#v-pills-${
+    }')" class="nav-link ${i === 0 && "active"}
+     second-nav-item" id="v-pills-${
+       element.id
+     }-tab" data-bs-toggle="pill" data-bs-target="#v-pills-${
       element.id
     }" type="button" role="tab" aria-controls="v-pills-${
       element.id
     }" aria-selected="${i === 0 ? "true" : "false"}">${element.name}</button> `;
   }
-  secondNav.innerHTML = `<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">${str}</div>`;
+  secondNav.innerHTML += `<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">${str}</div>`;
 }
 secondNavUI();
+
+function stateButton(params) {
+  let secondNav = document.querySelector(".second-nav");
+  secondNav.innerHTML += `
+  <div class="d-flex flex-nowrap">
+  <button class="btn btn-primary w-50 mx-1">預覽文章</button>
+  <button class="btn btn-danger w-50 mx-1 ">儲存文章</button>
+  </div>`;
+}
+stateButton();
--- js/twPartitionHandler.js
+++ js/twPartitionHandler.js
@@ -1,19 +1,92 @@
-// import jsonData from '../twPartition.json';
-// console.log('jsonData', jsonData)
-
-const xhr = new XMLHttpRequest();
-xhr.open('GET', '../twPartition.json');
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    const jsonData = JSON.parse(xhr.responseText);
-    console.log(jsonData,"jos")
-    // 现在，jsonData 对象包含从 data.json 文件解析的数据
-  } else {
-    console.error('请求数据失败！');
-  }
-};
-xhr.send();
+// 開關
 function cityZoneHandler() {
-    let dom=document.querySelector(".cityzone-selectContainer")
-    dom.classList.toggle("active")
+  let dom = document.querySelector(".cityzone-selectContainer");
+  dom.classList.toggle("active");
+}
+
+fetch("../twPartition.json")
+  .then((response) => response.json())
+  .then((jsonData) => {
+    searchCityUIRender(jsonData);
+  })
+  .catch((error) => {
+    console.error("JSON失敗", error);
+  });
+let locationObj = {
+  area: "東部",
+  city: "臺東縣",
+  district: "不分區",
+};
+function searchCityUIRender(data) {
+  // retur
+  const areaArr = Object.keys(data);
+  areaUI(areaArr);
+
+  Object.keys(data).forEach((key) => {
+    let area = null;
+
+    if (key === locationObj.area) {
+      area = data[key];
+      cityUI(Object.keys(area));
+    } else {
+      // 不是北區就不要走了
+      return;
+    }
+    Object.keys(area).forEach((key) => {
+      let city = null;
+      if (key === locationObj.city) {
+        city = area[key];
+        districtUI(Object.keys(city));
+      } else {
+        return;
+      }
+    });
+  });
+  return;
+}
+function areaUI(area) {
+  let dom = document.querySelector(".cityzone-selectContainer .area");
+  let str = "";
+  area.forEach((item) => {
+    str += `<div data-id="${item}" onclick="areaChange('${item}')" class="mainZone-item normal ${
+      item === `${locationObj.area}` ? "active" : ""
+    }">${item}</div>`;
+  });
+  dom.innerHTML = str;
+}
+
+function cityUI(city) {
+  let dom = document.querySelector(".cityzone-selectContainer .city");
+  let str = "";
+  city.forEach((item) => {
+    str += `<div data-id="${item}" onclick="cityChange('${item}')" class="minZone-item normal ${
+      item === `${locationObj.city}` ? "active" : ""
+    }">${item}</div>`;
+  });
+
+  dom.innerHTML = str;
+}
+
+function districtUI(district) {
+  let dom = document.querySelector(".cityzone-selectContainer .district");
+  let str = "";
+  district.forEach((item) => {
+    str += `<div data-id="${item}" onclick="districtChange('${item}')" class="minZone-item normal ${
+      item === `${locationObj.district}` ? "active" : ""
+    }">${item}</div>`;
+  });
+  dom.innerHTML = `<div data-id="不分區"  onclick="districtChange('不分區')"  class="minZone-item normal ${locationObj.district==='不分區' ?'active':''}">不分區</div>
+<div class="d-flex flex-wrap district-list">
+${str}
+</div>`;
+}
+
+function areaChange(areaName) {
+  console.log(areaName,"area")
+}
+function cityChange(cityName) {
+  console.log(cityName,"cityName")
+}
+function districtChange(districtName) {
+  console.log(districtName,"districtName")
 }

7d4e4f56047562bb4b6fb082b2e415fe2c284398
leeyang19970916 <yang19970916@gmail.com>
2023-05-24 17:57:59 +0800

    list is finished

--- component/Header.js
+++ component/Header.js
@@ -1,6 +1,6 @@
 function Header(params) {
-    let dom=document.querySelector("header")
-    let str=`
+  let dom = document.querySelector("header");
+  let str = `
     <div class="header-left">
       <div><button class="btn btn-warning">title</button></div>
       <div class="header-nav">
@@ -11,8 +11,10 @@ function Header(params) {
       </div>
     </div>
     <div class="header-right">
+    <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)">
+
       <button class="btn btn-primary text-white">追蹤臉書</button>
       <button class="btn btn-success text-white">加入LINE</button>
-    </div>`
-    dom.innerHTML+=str
+    </div>`;
+  dom.innerHTML += str;
 }

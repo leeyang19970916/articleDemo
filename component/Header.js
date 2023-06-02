1e5740acc9e097e1c60039acaf44066f37393282
leeyang19970916 <yang19970916@gmail.com>
2023-06-02 16:22:04 +0800

    link rwd

--- component/Header.js
+++ component/Header.js
@@ -4,8 +4,8 @@ function Header(params) {
     <div class="header-left">
       <div><button class="btn btn-warning">title</button></div>
       <div class="header-nav">
-        <div class="header-nav-item">預約掛號</div>
-        <div class="header-nav-item">健康報</div>
+        <div class="header-nav-item" onclick="redirectTo('reservation')" >預約掛號</div>
+        <div class="header-nav-item" onclick="redirectTo('article')" >健康報</div>
         <div class="header-nav-item">商城</div>
         <div class="header-nav-item">APP下載</div>
       </div>
@@ -20,4 +20,14 @@ function Header(params) {
       <button class="btn btn-success text-white">加入LINE</button>
     </div>`;
   dom.innerHTML += str;
+}
+
+function redirectTo(page) {
+  console.log(window.location.href,"window.location")
+  if (page==="reservation") {
+    window.location.href = "/front_desk/reservation/reservation.html"
+  }else if (page==="article") {
+    window.location.href = "/front_desk/healthPage_home.html";
+  }
+  
 }
